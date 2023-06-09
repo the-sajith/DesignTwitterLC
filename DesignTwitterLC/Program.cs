9b32e171931b6ec14abd6960ac6502783fdf6d45
Sajith K <the.sajith.k@gmail.com>
2023-06-09 13:49:36 +0530

    Add project files.

---
+++ DesignTwitterLC/Program.cs
@@ -0,0 +1,51 @@
+﻿namespace DesignTwitterLC
+{
+    /**
+     *  Design a simplified version of Twitter where users can post tweets, follow/unfollow another user, and is able to see the 10 most recent tweets in the user's news feed.
+
+        Implement the Twitter class:
+
+        Twitter() Initializes your twitter object.
+        void postTweet(int userId, int tweetId) Composes a new tweet with ID tweetId by the user userId. 
+        Each call to this function will be made with a unique tweetId.
+        List<Integer> getNewsFeed(int userId) Retrieves the 10 most recent tweet IDs in the user's news feed. 
+        Each item in the news feed must be posted by users who the user followed or by the user themself. 
+        Tweets must be ordered from most recent to least recent.
+        void follow(int followerId, int followeeId) The user with ID followerId started following the user with ID followeeId.
+        void unfollow(int followerId, int followeeId) The user with ID followerId started unfollowing the user with ID followeeId.
+     * Your Twitter object will be instantiated and called as such:
+     * Twitter obj = new Twitter();
+     * obj.PostTweet(userId,tweetId);
+     * IList<int> param_2 = obj.GetNewsFeed(userId);
+     * obj.Follow(followerId,followeeId);
+     * obj.Unfollow(followerId,followeeId);
+     */
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Twitter twitter = new Twitter();
+
+            twitter.PostTweet(1, 5);
+            twitter.PostTweet(1, 3);
+            twitter.PostTweet(1, 101);
+            twitter.PostTweet(1, 13);
+            twitter.PostTweet(1, 10);
+            twitter.PostTweet(1, 2);
+            twitter.PostTweet(1, 94);
+            twitter.PostTweet(1, 505);
+            twitter.PostTweet(1, 333);
+            twitter.PostTweet(1, 22);
+            twitter.PostTweet(1, 11);
+
+            var recentTweets = twitter.GetNewsFeed(1);
+
+            foreach (var tweet in recentTweets)
+            {
+                Console.WriteLine(tweet);
+            }
+
+        }
+    }
+}
---
+++ DesignTwitterLC/Twitter.cs
@@ -0,0 +1,93 @@
+﻿using System.Linq;
+
+public class Twitter
+{
+    public List<Tweet> Tweets { get; set; }
+
+    public HashSet<User> Users { get; set; }
+
+    public Twitter()
+    {
+
+        Tweets = new List<Tweet>();
+        Users = new HashSet<User>();
+    }
+
+    public void PostTweet(int userId, int tweetId)
+    {
+        var tweet = Tweets.Where(t => t.Id == tweetId).Any();
+
+        if (!tweet)
+        {
+            Tweets.Add(new Tweet { Id = tweetId, UserId = userId , TweetTime = DateTime.Now});
+
+            Users.Add(new User { Id = userId, });
+            
+        }
+    }
+
+    public IList<int> GetNewsFeed(int userId)
+    {
+        var user = Users.Where(user => user.Id == userId).FirstOrDefault();
+
+        if (user is null)
+        { 
+          return new List<int>();
+        }
+
+        var recentTweets = Tweets
+            .Where(tweet => tweet.UserId == userId)
+            .ToList();
+
+        foreach (var followee in user.Follows)
+        {
+            var tweets = Tweets.Where(tweet => tweet.UserId == followee);
+            recentTweets.AddRange(tweets);
+        }
+
+        return recentTweets
+            .OrderByDescending(t => t.TweetTime)
+            .Select(x => x.Id)
+            .Take(10)
+            .ToList();
+    }
+
+    public void Follow(int followerId, int followeeId)
+    {
+        Users.Add(new User { Id = followerId});
+        
+        var user = Users
+            .Where(user => user.Id == followerId)
+            .FirstOrDefault();
+
+        user.Follows.Add(followeeId);
+        
+    }
+
+    public void Unfollow(int followerId, int followeeId)
+    {
+        var user = Users
+            .Where(user => user.Id == followerId)
+            .FirstOrDefault();
+
+        user.Follows.Remove(followeeId);
+    }
+}
+
+public class Tweet
+{
+    public int Id { get; set; }
+
+    public int UserId { get; set; }
+
+    public DateTime TweetTime { get; set; }
+}
+
+public class User
+{
+    public int Id { get; set; }
+
+    public HashSet<int> Follows { get; set; } = new();
+
+}
+
